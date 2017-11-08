cce1df3a497ba6cc0a39234a1defbb1e8cbebc72
João Victor Rodrigues da Silva <victor.joao.1611@gmail.com>
2017-11-08 04:28:29 -0200

    Criação da tela de todos os itens

---
+++ EasyShop/app/src/main/java/com/mp20172/easyshop/AllItemsOfListActivity.kt
@@ -0,0 +1,12 @@
+package com.mp20172.easyshop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AllItemsOfListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_items_of_list)
+    }
+}
