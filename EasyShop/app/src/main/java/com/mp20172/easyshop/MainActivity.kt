bc924817a29b7146f192721b898809f2485af542
João Victor Rodrigues da Silva <victor.joao.1611@gmail.com>
2017-11-08 03:17:17 -0200

    Criação da tela principal

--- EasyShop/app/src/main/java/com/mp20172/easyshop/MainActivity.kt
+++ EasyShop/app/src/main/java/com/mp20172/easyshop/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.mp20172.easyshop
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
