a5f5a1419b7e0a6da77d14f7bdb1c84a92f5d323
João Victor <victor.joao.1611@gmail.com>
2017-11-08 20:47:51 -0200

    Criação da tela de cadastro de item

---
+++ EasyShop/app/src/main/java/com/mp20172/easyshop/RegisterItemActivity.kt
@@ -0,0 +1,12 @@
+package com.mp20172.easyshop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_item)
+    }
+}
