2503dbecb1dc582d3720fa17ea7c75ed6eb907ff
Daniel Marcos <wisebdan@gmail.com>
2017-12-05 00:53:08 -0200

    Makefile e arquivos iniciais

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#ifndef TEST
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv){
+     cout << "Hello World" << endl;
+     return 0;
+}
+
+#endif
